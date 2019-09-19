49cc0ec26bcab8d45d67860a1d9fc9cf88185714
Samuel Barata <oliveira3@icloud.comm>
2019-09-19 11:22:45 +0100

    Hello World

---
+++ Hello World/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+/**
+ *  Compilar:
+ *      javac HelloWorld.java
+ *  Correr:
+ *      java HelloWorld
+ * 
+ *      javac HelloWorld.java && java HelloWorld && rm HelloWorld.class
+ */
