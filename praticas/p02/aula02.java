fc4f53feb4936309c7b2bc91a070e16e0fe23354
Samuel Barata <oliveira3@icloud.com>
2019-09-23 14:47:53 +0100

    Makefile lab02

--- praticas/p02/aula02.java
+++ praticas/p02/aula02.java
@@ -1,8 +1,8 @@
 public class aula02{
     public static void main(String[] args){
         //ex01();
-        ex02();
-        //ex03();
+        //ex02();
+        ex03();
     }
 
     public static void ex01(){
@@ -18,12 +18,17 @@ public class aula02{
     }
 
     public static void ex02(){
-        car c1 = new car("BMW", 240);
+        pneu p1 = new pneu(2.2, 2.2);
+        pneu p2 = new pneu(2.2, 2.2);
+        pneu p3 = new pneu(2.2, 2.2);
+        pneu p4 = new pneu(2.2, 2.2);
+        car c1 = new car("BMW", 240, p1, p2, p3, p4);
         System.out.println(c1);
     }
 
     public static void ex03(){
-        
+        caneta c1 = new caneta(100, "blue", "bic");
+        c1.escrever("ola");
     }
 
 }
