398c20d167bf0bf7e55b178c0657d441d6cee4d5
Samuel Barata <oliveira3@icloud.com>
2019-11-12 02:07:58 +0000

    por hoje ja chega

--- projeto/p2/m19/app/main/DoSave.java
+++ projeto/p2/m19/app/main/DoSave.java
@@ -7,7 +7,6 @@ import m19.core.exception.MissingFileAssociationException;
 
 import pt.tecnico.po.ui.Command;
 import pt.tecnico.po.ui.Input;
-import pt.tecnico.po.ui.Display;
 
 // FIXME import other core concepts
 // FIXME import other ui concepts
@@ -18,31 +17,25 @@ import pt.tecnico.po.ui.Display;
 public class DoSave extends Command<LibraryManager> implements Message{
   
 	private Input<String> _inputForm;
-	private Display _display;
 
 	/**
 	 * @param receiver
 	 */
 	public DoSave(LibraryManager receiver) {
 		super(Label.SAVE, receiver);
-		_display = new Display();
-		_inputForm = _form.addStringInput(Message.saveAs());
+		_inputForm = _form.addStringInput(Message.newSaveAs());
 	}
 
 	/** @see pt.tecnico.po.ui.Command#execute() */
 	@Override
 	public final void execute() {
 		String _filename;
-		_display.clear();
-		_form.parse();					//pede ao utilizador o form construido acima
-		_filename=_inputForm.value();	//devolve o primeiro valor lido pelo form
+		_form.parse(false);
+		//TODO: ha outra mensagem pra usar quando o form ja tem valor?? idk mas algo aqui ainda n ta certo
+		_filename=_inputForm.value();
 
 		try{
-			if(_filename.equals(null)){
-				_display.add(Message.newSaveAs());
-			} else {
-				LibraryManager.saveAs(_filename);
-			}
+			LibraryManager.saveAs(_filename);
 		} catch(MissingFileAssociationException | IOException ex) {
 
 		}
